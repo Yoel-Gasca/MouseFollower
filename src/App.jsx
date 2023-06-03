380f99606514640c7263dc4a9d0c5cf136095297
Yoel-Gasca <83617933+Yoel-Gasca@users.noreply.github.com>
2023-06-02 21:28:34 -0600

    Cleaned UseEffect

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import { useEffect, useState } from "react";
-function App() {
+
+const FollowMouse = () => {
   const [enabled, setEnabled] = useState(false);
   const [position, setPosition] = useState({ x: 0, y: 0 })
 
@@ -7,16 +8,24 @@ function App() {
   useEffect(() => {
     console.log('effect', { enabled });
 
+    // Da seguimiento a la posición del puntero
     const handleMove = (event) => {
       const { clientX, clientY } = event;
       setPosition ({ x: clientX, y: clientY});
-    }
+    };
 
+    //Habilida el evento que permite seguir al puntero
     if (enabled) {
       window.addEventListener('pointermove', handleMove)
-    }
-  }, [enabled]); 
+    };
 
+    // Limpia la ejecucion del evento lo que permite desabilitar el seguimiento del puntero
+    return () => {
+      window.removeEventListener('pointermove', handleMove);
+    };
+
+  }, [enabled]); 
+  
   return (
    <>
     <main>
@@ -40,6 +49,15 @@ function App() {
         </button>
       </main>
     </>
+  )
+}
+function App() {
+  const [mounted, setMounted] = useState(true);
+
+  return (
+    <main>
+       <FollowMouse />
+    </main>
   );
 };
 
--- src/main.jsx
+++ src/main.jsx
@@ -4,6 +4,7 @@ import App from './App.jsx'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
+  //verifica que efecto se aplique de forma correcta
   <React.StrictMode>
     <App />
   </React.StrictMode>,
